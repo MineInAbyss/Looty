efcb88c1ef38f87b2f8e4c180f0cffdefeff2939
0ffz <dan.voznyy@gmail.com>
2020-11-27 17:49:13 -0500

    Add bukkit entity access to GearyExtensions

--- src/main/java/com/mineinabyss/looty/LootyECSRegistry.kt
+++ src/main/java/com/mineinabyss/looty/LootyECSRegistry.kt
@@ -2,15 +2,15 @@ package com.mineinabyss.looty
 
 import com.mineinabyss.geary.dsl.attachToGeary
 import com.mineinabyss.geary.ecs.GearyComponent
-import com.mineinabyss.geary.ecs.engine.Engine
-import com.mineinabyss.geary.ecs.serialization.Formats
+import com.mineinabyss.geary.ecs.components.with
 import com.mineinabyss.looty.config.LootyTypes
+import com.mineinabyss.looty.ecs.components.ChildItemCache
 import com.mineinabyss.looty.ecs.components.PotionComponent
 import com.mineinabyss.looty.ecs.components.Screaming
 import com.mineinabyss.looty.ecs.systems.ItemTrackerSystem
+import com.mineinabyss.looty.ecs.systems.ItemTrackerSystem.updateAndSaveItems
 import com.mineinabyss.looty.ecs.systems.PotionEffectSystem
 import com.mineinabyss.looty.ecs.systems.ScreamingSystem
-import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
 
@@ -21,10 +21,23 @@ fun Looty.attachToGeary() {
                 ScreamingSystem,
                 PotionEffectSystem,
         )
+
         serializers {
             polymorphic(GearyComponent::class) {
                 subclass(Screaming.serializer())
                 subclass(PotionComponent.serializer())
+            }
+        }
+
+        bukkitEntityAccess {
+            onPlayerRegister {
+                add(ChildItemCache())
+            }
+            onPlayerUnregister { gearyPlayer, player ->
+                gearyPlayer.with<ChildItemCache> {
+                    it.updateAndSaveItems(player.inventory, gearyPlayer)
+                    it.clear()
+                }
             }
         }
     }

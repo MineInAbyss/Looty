1cab8ddf4d359609acb7ebbb77ae2c419bf1b93f
0ffz <dan.voznyy@gmail.com>
2020-12-25 23:18:39 -0500

    Make slot types serializable

--- src/main/java/com/mineinabyss/looty/ecs/components/inventory/SlotType.kt
+++ src/main/java/com/mineinabyss/looty/ecs/components/inventory/SlotType.kt
@@ -1,9 +1,19 @@
 package com.mineinabyss.looty.ecs.components.inventory
 
 import com.mineinabyss.geary.ecs.GearyComponent
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 object SlotType {
+    @Serializable
+    @SerialName("looty:held")
     object Held: GearyComponent
+
+    @Serializable
+    @SerialName("looty:held")
     object Offhand: GearyComponent
+
+    @Serializable
+    @SerialName("looty:hotbar")
     object Hotbar: GearyComponent
 }

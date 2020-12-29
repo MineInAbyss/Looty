64248af9048e2fe3f3a91aa4234817933016f5c4
0ffz <dan.voznyy@gmail.com>
2020-12-29 00:04:15 -0500

    Improve explode action

--- src/main/java/com/mineinabyss/looty/ecs/actions/Explode.kt
+++ src/main/java/com/mineinabyss/looty/ecs/actions/Explode.kt
@@ -1,10 +1,9 @@
 package com.mineinabyss.looty.ecs.actions
 
 import com.mineinabyss.geary.ecs.GearyEntity
+import com.mineinabyss.geary.ecs.actions.AtPlayerLocation
+import com.mineinabyss.geary.ecs.actions.ConfigurableLocation
 import com.mineinabyss.geary.ecs.actions.GearyAction
-import com.mineinabyss.geary.ecs.components.parent
-import com.mineinabyss.geary.ecs.components.with
-import com.mineinabyss.geary.minecraft.components.PlayerComponent
 import com.mineinabyss.idofront.spawning.spawn
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -12,10 +11,22 @@ import org.bukkit.entity.TNTPrimed
 
 @Serializable
 @SerialName("looty:explode")
-class Explode : GearyAction() {
-    override fun runOn(entity: GearyEntity) {
-        entity.parent?.with<PlayerComponent> { (player) ->
-            player.location.spawn<TNTPrimed>()
-        }
+class Explode(
+    private val at: ConfigurableLocation = AtPlayerLocation(),
+    private val power: Float = 4F,
+    private val setFire: Boolean = false,
+    private val breakBlocks: Boolean = false,
+    private val fuseTicks: Int = 0
+) : GearyAction() {
+    override fun runOn(entity: GearyEntity): Boolean {
+        val location = at.get(entity) ?: return false
+
+        if (fuseTicks <= 0)
+            location.createExplosion(power, setFire, breakBlocks)
+        else //only spawn a tnt in if we have a fuse
+            location.spawn<TNTPrimed> {
+                fuseTicks = this@Explode.fuseTicks
+            }
+        return true
     }
 }

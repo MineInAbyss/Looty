1f36d1322aa1e4371b7d3cf88d7cc2539670f146
0ffz <dan.voznyy@gmail.com>
2021-01-13 12:46:18 -0500

    Use autoscanner

--- src/main/java/com/mineinabyss/looty/Looty.kt
+++ src/main/java/com/mineinabyss/looty/Looty.kt
@@ -1,11 +1,17 @@
 package com.mineinabyss.looty
 
+import com.mineinabyss.geary.ecs.components.with
+import com.mineinabyss.geary.minecraft.dsl.attachToGeary
 import com.mineinabyss.idofront.commands.execution.ExperimentalCommandDSL
 import com.mineinabyss.idofront.plugin.registerEvents
 import com.mineinabyss.looty.config.LootyAddon
+import com.mineinabyss.looty.config.LootyTypes
 import com.mineinabyss.looty.config.registerAddonWithLooty
+import com.mineinabyss.looty.ecs.components.ChildItemCache
 import com.mineinabyss.looty.ecs.components.events.LootyEventListener
-import com.mineinabyss.looty.ecs.systems.InventoryTrackingListener
+import com.mineinabyss.looty.ecs.systems.*
+import kotlinx.serialization.InternalSerializationApi
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 import kotlin.time.ExperimentalTime
@@ -16,6 +22,7 @@ val looty: Looty by lazy { JavaPlugin.getPlugin(Looty::class.java) }
 class Looty : JavaPlugin(), LootyAddon {
     override val relicsDir = File(dataFolder, "relics")
 
+    @InternalSerializationApi
     @ExperimentalCommandDSL
     @ExperimentalTime
     override fun onEnable() {
@@ -27,11 +34,34 @@ class Looty : JavaPlugin(), LootyAddon {
         LootyCommands()
 
         registerEvents(
-                InventoryTrackingListener,
-                LootyEventListener
+            InventoryTrackingListener,
+            LootyEventListener
         )
 
-        attachToGeary()
+        attachToGeary(types = LootyTypes) {
+            systems(
+                ItemTrackerSystem,
+                ScreamingSystem,
+                PotionEffectSystem,
+                CooldownDisplaySystem,
+            )
+
+            autoscanActions()
+            autoscanComponents()
+
+            bukkitEntityAccess {
+                onEntityRegister<Player> { player ->
+                    add(ChildItemCache(player))
+                }
+
+                onEntityUnregister<Player> { gearyPlayer, player ->
+                    gearyPlayer.with<ChildItemCache> {
+                        it.reevaluate(player.inventory)
+                        it.clear()
+                    }
+                }
+            }
+        }
 
         registerAddonWithLooty()
     }

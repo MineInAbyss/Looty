78f2b9bc02c5630b9bfa8bcabe9377429ef57693
Norazan <thendjes17@gmail.com>
2021-01-18 21:29:22 +0100

    Move some event stuff out to Geary

--- src/main/java/com/mineinabyss/looty/ecs/components/events/LootyEventListener.kt
+++ src/main/java/com/mineinabyss/looty/ecs/components/events/LootyEventListener.kt
@@ -1,15 +1,11 @@
 package com.mineinabyss.looty.ecs.components.events
 
-import com.mineinabyss.geary.ecs.GearyEntity
-import com.mineinabyss.geary.ecs.actions.CancelEventAction
-import com.mineinabyss.geary.ecs.components.get
+import com.mineinabyss.geary.minecraft.events.event
 import com.mineinabyss.geary.minecraft.store.get
 import com.mineinabyss.idofront.entities.leftClicked
 import com.mineinabyss.idofront.entities.rightClicked
 import com.mineinabyss.looty.ecs.components.ChildItemCache
 import org.bukkit.entity.Player
-import org.bukkit.event.Cancellable
-import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerDropItemEvent
@@ -17,14 +13,6 @@ import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerItemBreakEvent
 
 val Player.heldLootyItem get() = get<ChildItemCache>()?.get(inventory.heldItemSlot)
-
-fun Event.event(entity: GearyEntity?, name: String) {
-    entity?.get<Events>()?.wrapped?.get(name)?.forEach {
-        it.runOn(entity)
-        if (it is CancelEventAction && this is Cancellable)
-            isCancelled = true
-    }
-}
 
 object LootyEventListener : Listener {
     @EventHandler

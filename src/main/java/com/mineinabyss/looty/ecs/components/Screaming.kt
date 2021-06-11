c093e2beb8b5fe5fc66af4ecf26b21093b0966aa
SaloEater <danil.lazarev@gmail.com>
2021-06-12 00:49:14 +0300

    Optimize imports

--- src/main/java/com/mineinabyss/looty/ecs/components/Screaming.kt
+++ src/main/java/com/mineinabyss/looty/ecs/components/Screaming.kt
@@ -1,15 +1,8 @@
 package com.mineinabyss.looty.ecs.components
 
 import com.mineinabyss.geary.ecs.api.autoscan.AutoscanComponent
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 @SerialName("debug:screaming")
@@ -17,4 +10,3 @@ import kotlinx.serialization.encoding.Encoder
 data class Screaming(
     val scream: String = "AAAAAAAAAAAA"
 )
-

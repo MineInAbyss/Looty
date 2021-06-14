a0c0cb27557e2ff0f0885943d16f924708a9ac46
SaloEater <danil.lazarev@gmail.com>
2021-06-14 22:45:50 +0300

    Fixes

--- src/main/java/com/mineinabyss/looty/ecs/systems/PutInABrokenStateOnDurabilityDepleteSystem.kt
+++ src/main/java/com/mineinabyss/looty/ecs/systems/PutInABrokenStateOnDurabilityDepleteSystem.kt
@@ -1,7 +1,6 @@
 package com.mineinabyss.looty.ecs.systems
 
 import com.mineinabyss.geary.ecs.api.entities.GearyEntity
-import com.mineinabyss.geary.minecraft.access.BukkitAssociations
 import com.mineinabyss.looty.ecs.components.DurabilityDepletedComponent
 import com.mineinabyss.looty.ecs.components.PlayerInventoryContext
 import com.mineinabyss.looty.ecs.components.PutInABrokenStateDurabilityDepleteComponent
@@ -10,11 +9,10 @@ import com.mineinabyss.looty.ecs.components.UpdateContextItemComponent
 object PutInABrokenStateOnDurabilityDepleteSystem : LootyItemSystem() {
     private val durabilityDepletedComponent = has<DurabilityDepletedComponent>()
     private val putInABrokenStateDurabilityDepleteComponent by get<PutInABrokenStateDurabilityDepleteComponent>()
+    private val context by get<PlayerInventoryContext>()
 
     override fun GearyEntity.tick() {
-        val entity = BukkitAssociations[uuid] ?: return
-        val context = entity.get<PlayerInventoryContext>()
-        context?.item?.let {
+        context.item?.let {
             if (!lootyType.item.type.isItem) return
             set(UpdateContextItemComponent(putInABrokenStateDurabilityDepleteComponent.item))
         }
--- src/main/java/com/mineinabyss/looty/ecs/systems/UpdateContextItemSystem.kt
+++ src/main/java/com/mineinabyss/looty/ecs/systems/UpdateContextItemSystem.kt
@@ -1,17 +1,15 @@
 package com.mineinabyss.looty.ecs.systems
 
 import com.mineinabyss.geary.ecs.api.entities.GearyEntity
-import com.mineinabyss.geary.minecraft.access.BukkitAssociations
 import com.mineinabyss.looty.ecs.components.PlayerInventoryContext
 import com.mineinabyss.looty.ecs.components.UpdateContextItemComponent
 
 object UpdateContextItemSystem : LootyItemSystem() {
     private val updateContextItemComponent by get<UpdateContextItemComponent>()
+    private val context by get<PlayerInventoryContext>()
 
     override fun GearyEntity.tick() {
-        val entity = BukkitAssociations[uuid] ?: return
-        val context = entity.get<PlayerInventoryContext>()
-        context?.item?.let {
+        context.item?.let {
             val oldMeta = it.itemMeta
             val item = updateContextItemComponent.item.toItemStack().clone()
             item.itemMeta = oldMeta

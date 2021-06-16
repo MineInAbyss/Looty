6872fdfd4cb702e89cf5660c521c910e23d23b8f
SaloEater <danil.lazarev@gmail.com>
2021-06-16 09:51:26 +0300

    Sync with geary

--- src/main/java/com/mineinabyss/looty/ecs/systems/DurabilityDepletedSystem.kt
+++ src/main/java/com/mineinabyss/looty/ecs/systems/DurabilityDepletedSystem.kt
@@ -9,7 +9,7 @@ import com.mineinabyss.looty.ecs.components.MinDurabilityComponent
 object DurabilityDepletedSystem : LootyItemSystem() {
     private val durabilityComponent by get<DurabilityComponent>()
     private val minDurabilityComponent by get<MinDurabilityComponent>()
-    private val brokenComponent = hasNot<BrokenComponent>()
+    private val brokenComponent = lacks<BrokenComponent>()
 
     override fun GearyEntity.tick() {
         if (durabilityComponent.durability == minDurabilityComponent.minDurability) {

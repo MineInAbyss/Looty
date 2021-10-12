4f1d5b3dc3475978613b6e970c36556985f493af
Scyu_ <scyu@scyu.dev>
2021-10-13 00:49:50 +0100

    Update recipes

--- src/main/java/com/mineinabyss/looty/ecs/components/RegisterRecipeComponent.kt
+++ src/main/java/com/mineinabyss/looty/ecs/components/RegisterRecipeComponent.kt
@@ -1,6 +1,7 @@
 package com.mineinabyss.looty.ecs.components
 
 import com.mineinabyss.geary.ecs.api.autoscan.AutoscanComponent
+import com.mineinabyss.idofront.serialization.SerializableItemStack
 import com.mineinabyss.idofront.serialization.recipes.SerializableRecipeIngredients
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -12,5 +13,6 @@ class RegisterRecipeComponent(
     val recipes: List<SerializableRecipeIngredients>,
     val discoverRecipes: Boolean = false,
     val group: String = "",
-    val removeRecipes: List<String> = listOf()
+    val removeRecipes: List<String> = listOf(),
+    val result: SerializableItemStack? = null,
 )
--- src/main/java/com/mineinabyss/looty/ecs/systems/ItemRecipeSystem.kt
+++ src/main/java/com/mineinabyss/looty/ecs/systems/ItemRecipeSystem.kt
@@ -7,21 +7,29 @@ import com.mineinabyss.idofront.recipes.register
 import com.mineinabyss.looty.LootyFactory
 import com.mineinabyss.looty.ecs.components.LootyType
 import com.mineinabyss.looty.ecs.components.RegisterRecipeComponent
+import com.mineinabyss.looty.looty
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.inventory.ItemStack
 
 class ItemRecipeSystem : TickingSystem(), Listener {
     private val QueryResult.recipes by get<RegisterRecipeComponent>()
-    private val QueryResult.type by get<LootyType>()
     private val QueryResult.prefabKey by get<PrefabKey>()
     private val registeredRecipes = mutableSetOf<NamespacedKey>()
     private val discoveredRecipes = mutableSetOf<NamespacedKey>()
 
     override fun QueryResult.tick() {
-        val result = LootyFactory.createFromPrefab(prefabKey) ?: return
+        val result: ItemStack = if (entity.has<LootyType>()) {
+            LootyFactory.createFromPrefab(prefabKey) ?: return
+        } else {
+            recipes.result?.toItemStack() ?: run {
+                looty.logger.warning("Recipe ${prefabKey.name} is missing result item")
+                return
+            }
+        }
 
         recipes.removeRecipes.forEach {
             Bukkit.removeRecipe(NamespacedKey.fromString(it)!!)

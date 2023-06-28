d393452b0f61d02cd441d9b1a38e6adffa30494d
Danielle Voznyy <dan.voznyy@gmail.com>
2023-06-28 10:20:32 -0400

    Fix disable interactions system

--- src/main/kotlin/com/mineinabyss/looty/features/nointeraction/DisableItemInteractionsSystem.kt
+++ src/main/kotlin/com/mineinabyss/looty/features/nointeraction/DisableItemInteractionsSystem.kt
@@ -6,7 +6,7 @@ import com.mineinabyss.geary.datatypes.family.family
 import com.mineinabyss.geary.papermc.bridge.components.Interacted
 import com.mineinabyss.geary.systems.GearyListener
 import com.mineinabyss.geary.systems.accessors.EventScope
-import com.mineinabyss.geary.systems.accessors.SourceScope
+import com.mineinabyss.geary.systems.accessors.TargetScope
 import com.mineinabyss.geary.systems.accessors.building.map
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
@@ -15,7 +15,7 @@ import org.bukkit.event.Event
 class DisableItemInteractionsSystem : GearyListener() {
     private val EventScope.bukkit by get<Event>().map { it as? Cancellable }
     private val EventScope.interacted by family { has<Interacted>() }
-    private val SourceScope.noVanilla by family { has<DisableItemInteractions>() }
+    private val TargetScope.noVanilla by family { has<DisableItemInteractions>() }
 
     @Handler
     fun onPlace(event: EventScope) {

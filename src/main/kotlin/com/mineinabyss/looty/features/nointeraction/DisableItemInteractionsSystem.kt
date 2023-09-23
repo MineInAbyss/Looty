1fa9167dc0ed84268eb305f17a569b8fb8873d60
Boy <sivertpaulsen2@gmail.com>
2023-09-23 20:16:16 +0200

    update to geary-23

--- src/main/kotlin/com/mineinabyss/looty/features/nointeraction/DisableItemInteractionsSystem.kt
+++ src/main/kotlin/com/mineinabyss/looty/features/nointeraction/DisableItemInteractionsSystem.kt
@@ -1,25 +1,21 @@
 package com.mineinabyss.looty.features.nointeraction
 
-import com.mineinabyss.geary.annotations.Handler
 import com.mineinabyss.geary.autoscan.AutoScan
 import com.mineinabyss.geary.datatypes.family.family
 import com.mineinabyss.geary.papermc.bridge.components.Interacted
 import com.mineinabyss.geary.systems.GearyListener
-import com.mineinabyss.geary.systems.accessors.EventScope
-import com.mineinabyss.geary.systems.accessors.TargetScope
-import com.mineinabyss.geary.systems.accessors.building.map
+import com.mineinabyss.geary.systems.accessors.Pointers
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 
 @AutoScan
 class DisableItemInteractionsSystem : GearyListener() {
-    private val EventScope.bukkit by get<Event>().map { it as? Cancellable }
-    private val EventScope.interacted by family { has<Interacted>() }
-    private val TargetScope.noVanilla by family { has<DisableItemInteractions>() }
+    private val Pointers.bukkit by get<Event>().map { (it as? Cancellable) }.on(event)
+    private val Pointers.interacted by family { has<Interacted>() }.on(event)
+    private val Pointers.noVanilla by family { has<DisableItemInteractions>() }.on(target)
 
-    @Handler
-    fun onPlace(event: EventScope) {
-        event.bukkit?.isCancelled = true
+    override fun Pointers.handle() {
+        bukkit?.isCancelled = true
     }
 }
 

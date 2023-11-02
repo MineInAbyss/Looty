d3b7ec9059b80e78e517554108842cd5d2cb18b4
Danielle Voznyy <dan.voznyy@gmail.com>
2023-11-01 21:51:55 -0400

    Update to paper-plugin, bump idofront, geary

--- src/main/kotlin/com/mineinabyss/looty/LootyPlugin.kt
+++ src/main/kotlin/com/mineinabyss/looty/LootyPlugin.kt
@@ -4,7 +4,6 @@ package com.mineinabyss.looty
 import com.mineinabyss.geary.autoscan.autoscan
 import com.mineinabyss.geary.modules.geary
 import com.mineinabyss.idofront.di.DI
-import com.mineinabyss.idofront.platforms.Platforms
 import com.mineinabyss.idofront.plugin.listeners
 import com.mineinabyss.idofront.plugin.service
 import com.mineinabyss.idofront.serialization.SerializablePrefabItemService
@@ -20,14 +19,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class LootyPlugin : JavaPlugin() {
     override fun onLoad() {
-        Platforms.load(this, "mineinabyss")
-    }
-
-    override fun onEnable() {
         DI.add(LootyModule(this))
-
-        //Reset to avoid duplicates and clear mixes that have been removed
-        server.potionBrewer.resetPotionMixes()
 
         geary {
             autoscan(classLoader, "com.mineinabyss.looty") {
@@ -40,6 +32,13 @@ class LootyPlugin : JavaPlugin() {
                 PotionMixRecipeSystem(),
             )
         }
+    }
+
+    override fun onEnable() {
+
+        //Reset to avoid duplicates and clear mixes that have been removed
+        server.potionBrewer.resetPotionMixes()
+
         service<SerializablePrefabItemService>(LootySerializablePrefabItemService())
         LootyCommands() //Register commands
 
